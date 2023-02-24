a5f14a09eb13edca38402e6b68abfb7eb095c2e5
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-02-24 20:01:04 +0500

    INFRA:Initialize Unit Test Project

---
+++ CashOverflow.Clone.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace CashOverflow.Clone.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
