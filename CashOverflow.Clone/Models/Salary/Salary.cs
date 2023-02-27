df15cdec752a020b226d76ee708292cdfc4d801b
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-02-27 17:13:50 +0500

    DATA: Model & Migrate Salary

---
+++ CashOverflow.Clone/Models/Salary/Salary.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+
+namespace CashOverflow.Clone.Models.Salary;
+
+public class Salary
+{
+    public Guid Id { get; set; }
+    public decimal Amount { get; set; }
+    public int Experience { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public Guid LocationId { get; set; }
+    public Guid LanguageId { get; set; }
+    public Guid JobId { get; set; }
+    
+}
