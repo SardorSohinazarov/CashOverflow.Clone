02ebdf7b90e7b8e706dd8d979945e38148258312
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-02-27 17:31:25 +0500

    DATA: Model & Migrate Job

---
+++ CashOverflow.Clone/Models/Job/Job.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using System;
+
+namespace CashOverflow.Clone.Models.Job;
+
+public class Job
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public Level Level { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset UpdatedDate { get; set; }
+}
---
+++ CashOverflow.Clone/Models/Job/Level.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+namespace CashOverflow.Clone.Models.Job;
+
+public enum Level
+{
+    Other,
+    Junior,
+    Middle,
+    Senior
+}
