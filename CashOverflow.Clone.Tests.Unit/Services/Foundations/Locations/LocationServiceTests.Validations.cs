a93f958cf7da88a8af27bb5ed988d10f8972cdec
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-03-05 04:02:18 +0500

    ShouldThrowValidationExceptionOnAddIfInputIsNullandLogItAsync -> WIP

---
+++ CashOverflow.Clone.Tests.Unit/Services/Foundations/Locations/LocationServiceTests.Validations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashOverflow.Clone.Tests.Unit.Services.Foundations.Locations
+{
+    public class LocationServiceTests
+    {
+    }
+}
